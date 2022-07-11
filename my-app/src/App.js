02ca7b1567cd81f01ecf20fc6ebd72f19a5de639
ericruga <ericruga@users.noreply.github.com>
2022-07-11 15:56:05 +0200

    Some changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,23 +13,23 @@ function App() {
     <img src={logo}/>
     <img src={menu}/>
     <h1>Say hello to ReactJS</h1>
-    <h3>You will learn how to use the most popular frontend library, and become a super Ninja developer</h3>
+    <p>You will learn how to use the most popular frontend library, and become a super Ninja developer</p>
   </div>
   <div className="Second">
     <div className="Second1">
-      <img src={icon1}/>
+      <img src={icon1} max-width="20px" max-heigth="20px" alt="icon1"/>
       <p>React makes it painless to create interactive UIs.</p>
     </div>
     <div className="Second2">
-      <img src={icon2}/>
+    <img src={icon2} max-width="20px" max-heigth="20px" alt="icon2"/>
       <p>Build encapsulated components that manage their state.</p>
     </div>
     <div className="Second3">
-      <img src={icon3}/>
+    <img src={icon3} max-width="20px" max-heigth="20px" alt="icon3"/>
       <p>A set of immutable values are passed to the component's.</p>
     </div>
     <div className="Second4">
-      <img src={icon4}/>
+    <img src={icon4} max-width="20px" max-heigth="20px" alt="icon4"/>
       <p>Statically-typed, designed to run on modern browsers.</p>
     </div>
   
